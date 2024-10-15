4abaa8cc772ad73aa7e88233ad0611a7886afab2
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2024-10-15 08:52:57 +0300

    Add burn button finally to mainnet wallet.

--- build-run.sh
+++ build-run.sh
@@ -5,18 +5,21 @@ cd htn-core-lib
 npm install
 cd ..
 rm -rf htn-wallet-worker/node_modules
+rm -rf htn-pwa-wallet/dist
 rm -rf htn-wallet-worker/package-lock.json
 cd htn-wallet-worker
 npm install
 npm run prepublishOnly
 cd ..
 rm -rf htn-wallet/node_modules
+rm -rf htn-pwa-wallet/dist
 rm -rf htn-wallet/package-lock.json
 cd htn-wallet
 npm install
 npm run prepublishOnly
 cd ..
 rm -rf htn-pwa-wallet/node_modules
+rm -rf htn-pwa-wallet/dist
 rm -rf htn-pwa-wallet/package-lock.json
 cd htn-pwa-wallet
 npm install
