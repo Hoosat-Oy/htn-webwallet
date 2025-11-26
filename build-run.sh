9e49948f897a4cbd9f2f674e251dcd27518e92e5
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2025-11-26 21:24:47 +0200

    backtrack deps

--- build-run.sh
+++ build-run.sh
@@ -4,17 +4,17 @@ rm -rf htn-core-lib/package-lock.json
 cd htn-core-lib
 npm install
 cd ..
-rm -rf htn-wallet-worker/node_modules
-rm -rf htn-wallet-worker/dist
-rm -rf htn-wallet-worker/package-lock.json
-cd htn-wallet-worker
-npm install
-npm run prepublishOnly
-cd ..
 rm -rf htn-wallet/node_modules
 rm -rf htn-wallet/dist
 rm -rf htn-wallet/package-lock.json
 cd htn-wallet
+npm install
+npm run prepublishOnly
+cd ..
+rm -rf htn-wallet-worker/node_modules
+rm -rf htn-wallet-worker/dist
+rm -rf htn-wallet-worker/package-lock.json
+cd htn-wallet-worker
 npm install
 npm run prepublishOnly
 cd ..
